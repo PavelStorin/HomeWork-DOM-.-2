0c1b2cc09acaccb7e5a09042cfc599f30de2cf5f
PavelStorin <storin@inbox.ru>
2026-03-29 22:51:17 +0300

    editing faq_accordion

--- dom_base/faq_accordion/main.js
+++ dom_base/faq_accordion/main.js
@@ -2,13 +2,21 @@ const faq = document.getElementById("faq");
 const items = document.querySelectorAll(".faq-item");
 
 faq.addEventListener("click", (event) => {
+
     const question = event.target.closest(".question")
+
+    if (!question) return; 
+
+    const faqItem = question.closest(".faq-item");
+
+    const wasActive = faqItem.classList.contains("active");
 
     items.forEach((item)=> {
         item.classList.remove("active");
     })
 
-    const faqItem = question.closest(".faq-item");
-    faqItem.classList.add("active");
+    if (!wasActive) {
+        faqItem.classList.add("active");
+    }
 })
 
