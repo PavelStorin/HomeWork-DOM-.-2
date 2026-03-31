4acb1002e4ae52b55109be1d32784c56fdbab93a
PavelStorin <storin@inbox.ru>
2026-03-31 23:16:36 +0300

    completed Form Validation

--- dom_base/form_validation/main.js
+++ dom_base/form_validation/main.js
@@ -1,42 +1,34 @@
 const form = document.getElementById("registerForm");
 const inputs = form.querySelectorAll("input");
 
-inputs.forEach((input) => {
-  input.addEventListener("blur", () => validateField(input));
-});
-
-form.addEventListener("submit", (event) => {
+form.addEventListener('submit', (event) => {
   event.preventDefault();
 
   let isValid = true;
 
-  inputs.forEach((input) => {
-    const valid = validateField(input);
-    if (!valid) isValid = false;
+  inputs.forEach(input => {
+    const errorElement = input.parentElement.querySelector('.error');
+
+    if (!input.validity.valid) {
+    
+      isValid = false;
+      input.classList.add('invalid');
+      input.setAttribute('aria-invalid', 'true');
+      if (errorElement) errorElement.style.display = 'block'; 
+    } 
+    
+    else {
+      input.classList.remove('invalid');
+      input.setAttribute('aria-invalid', 'false');
+      if (errorElement) errorElement.style.display = 'none'; 
+    }
   });
 
-  if (!isValid) return;
-
-  const formData = new FormData(form);
-  const data = Object.fromEntries(formData.entries());
-
-  console.log(data);
-});
-
-function validateField(input) {
-  const field = input.closest(".field");
-  const error = field.querySelector(".error");
-
-  if (!input.validity.valid) {
-    error.style.display = "block";
-    input.classList.add("invalid");
-    input.setAttribute("aria-invalid", "true");
-    return false;
+  if (isValid) {
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries());
+    console.log('Данные:', data);
+  } else {
+    console.log('Не валидна');
   }
-
-  error.style.display = "none";
-  input.classList.remove("invalid");
-  input.setAttribute("aria-invalid", "false");
-
-  return true;
-}
+});
